af9936e3902861eafd00d63e8600453c9cba9c6a
smrianone95 <smrianone95@gmail.com>
2023-09-06 09:49:18 +0100

    Added some verification script

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var convertBtn = document.getElementById("convert-button");
 var URLinput = document.getElementById("URL-input");
+var invalid = document.getElementById("invalid");
+var regVideo = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+
 convertBtn.addEventListener("click", () => {
-  console.log(`URL: ${URLinput.value}`);
-  sendURL(URLinput.value);
+    url = URLinput.value;
+
+    if (url) {
+        if (url.match(regVideo)) {
+          console.log(`URL: ${url}`);
+          invalid.style.visibility = "hidden";
+          sendURL(url);
+        } else {
+          invalid.style.visibility = "visible";
+        }
+    }
+    else {
+        invalid.style.visibility = "visible";
+    }
 });
 
 
